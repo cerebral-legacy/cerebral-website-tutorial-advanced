86c95803c5ad9308c06b67f5b34ff6ca05aef155
Christian Alfoni <christianalfoni@gmail.com>
2016-06-15 13:25:13 +0200

    added items component

--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import {Decorator as Cerebral} from 'cerebral-view-react';
+import {Component, h} from 'cerebral-view-snabbdom'
 import filteredItems from '../../computed/filteredItems'
 import Item from '../Item'
 
-@Cerebral({
+export default Component('Items', {
   items: filteredItems
-})
-class Items extends React.Component {
-  render() {
-    return (
-      <ul>
-        {Object.keys(this.props.items).map(key => (
-          <Item key={key} itemKey={key}) />
-        ))}
-      </ul>
-    );
-  }
-}
-
- export default Items;
+}, props => (
+  h('ul',
+    Object.keys(props.items).map(key => (
+      Item({
+        key,
+        itemKey: key
+      })
+    ))
+  )
+))
